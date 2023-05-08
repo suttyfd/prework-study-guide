5438d42bbd709d43954c10e539bf783c88e19d4c
suttyfd <73559193+suttyfd@users.noreply.github.com>
2023-05-08 18:11:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-var topics = "HTML, CSS Git, Javascript";
-console.log(topics)
+var topic = "HTML";
+
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
